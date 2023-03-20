2671014e40834e24dcb9879743e11076e7663392
root <root@Sty-PC.localdomain>
2023-03-19 23:30:34 -0300

    initial Setup

---
+++ pages/searchtest.vue
@@ -0,0 +1,17 @@
+<script>
+import NavMenu from "@/components/NavMenu.vue";
+
+export default {
+  components: {
+    NavMenu,
+  },
+};
+</script>
+<template>
+<div id="wrapper">
+   <NavMenu> </NavMenu>
+<h1>Lydia</h1>
+</div>
+</template>
+
+<style></style>
---
+++ pages/teste2.vue
@@ -0,0 +1,25 @@
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+<template>
+  <div id="mama2">
+    <h2>MAMA mina</h2>
+    <h6>mama</h6>
+    <div id="mama3">
+      <h32>wewew</h32>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped></style>
